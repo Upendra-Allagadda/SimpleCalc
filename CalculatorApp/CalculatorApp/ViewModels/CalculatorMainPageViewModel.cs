deb187bf1459ad77bae8f6b666da290154cbbdde
Allagadda Sai Upendranath <sainath940@gmail.com>
2023-01-15 18:18:16 +0530

    Page design and skeletal backend code.
    Added the page design. Added View Model for the respective page.

---
+++ CalculatorApp/CalculatorApp/ViewModels/CalculatorMainPageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp.ViewModels
+{
+    public class CalculatorMainPageViewModel : INotifyPropertyChanged
+    {
+        private string result;
+        public string FinalResult
+        {
+            get
+            {
+                return result;
+            }
+            set
+            {
+                result = value;
+                OnPropertyChanged("FinalResult");
+            }
+        }
+
+        private string previousCalculation;
+        public string WholeCalculation
+        {
+            get
+            {
+                return previousCalculation;
+            }
+            set
+            {
+                previousCalculation = value;
+                OnPropertyChanged("WholeCalculation");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
